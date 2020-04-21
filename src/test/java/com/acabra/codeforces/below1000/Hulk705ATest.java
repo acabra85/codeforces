65044fb2e661d1d13c20c20bd910df67c88b55a4
Agustin Cabra <cabra.agustin@gmail.com>
2020-04-21 23:43:17 +0200

    [initial] initial commit maven bringing files from older repo

---
+++ src/test/java/com/acabra/codeforces/below1000/Hulk705ATest.java
@@ -0,0 +1,14 @@
+package com.acabra.codeforces.below1000;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Hulk705ATest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals("I hate that I love that I hate it", Hulk705A.Solution.solution(3));
+        Assert.assertEquals("I hate that I love it", Hulk705A.Solution.solution(2));
+        Assert.assertEquals("I hate it", Hulk705A.Solution.solution(1));
+    }
+}
